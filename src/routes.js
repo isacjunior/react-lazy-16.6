953cbbf9d641390aa286292d2285784f15c40b26
Isac <isac_asj91@hotmail.com>
2018-10-28 23:11:40 -0300

    create example using lazy with React 16.6

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+import Route from 'react-router-dom/Route'
+import Switch from 'react-router-dom/Switch'
+import LazyImport from './Components/LazyImport'
+
+const Nav = LazyImport(() => import('./Components/Nav'/* webpackChunkName: 'nav' */))
+
+const Main = LazyImport(() => import('./Components/Main'/* webpackChunkName: 'main' */))
+
+const Other = LazyImport(() => import('./Components/Other'/* webpackChunkName: 'other' */))
+
+const Routes = () => (
+  <BrowserRouter>
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/other" component={Other} />
+      </Switch>
+    </>
+  </BrowserRouter>
+)
+
+export default Routes
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+
+  devServer: {
+    historyApiFallback: true,
+  },
+
+  mode: 'development',
+
+  entry: {
+    app: './src/index.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader?cacheDirectory',
+        },
+      },
+    ],
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+}
